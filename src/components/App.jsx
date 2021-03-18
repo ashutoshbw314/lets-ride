a7096e63a7add2ac55e1d63bf98963ff2f05f7d7
Ashutosh Biswas <ashutoshbw314@gmail.com>
2021-03-19 05:31:15 +0600

    test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,13 +7,11 @@ import ReversedPrivateRoute from '../routes/ReversedPrivateRoute';
 
 function App() {
   return (
-    <ProvideAuth>
       <div>
         <h1 className="text-2xl text-center bg-blue-300"> Hello </h1>
       </div>
 
-    </ProvideAuth>
   )
 }
 
-export default App
+export default App;
