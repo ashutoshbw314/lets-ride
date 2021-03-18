a71ede485b119f599f84b0b54b9a251058d6f556
Ashutosh Biswas <ashutoshbw314@gmail.com>
2021-03-19 05:22:10 +0600

    configuring tailwind config

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { ProvideAuth } from '../hooks/useAuth';
+import PrivateRoute from '../routes/PrivateRoute';
+import ReversedPrivateRoute from '../routes/ReversedPrivateRoute';
+
 
 function App() {
   return (
-    <div>
-      <h1 className="text-2xl text-center bg-blue-300"> Hello </h1>
-    </div>
+    <ProvideAuth>
+      <div>
+        <h1 className="text-2xl text-center bg-blue-300"> Hello </h1>
+      </div>
+
+    </ProvideAuth>
   )
 }
 
