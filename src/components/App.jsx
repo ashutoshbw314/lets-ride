f0b13c4f073ef463dc01b83e15dd8c4f3c9ffee8
Ashutosh Biswas <ashutoshbw314@gmail.com>
2021-03-20 06:49:05 +0600

    test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import { ProvideAuth } from '../hooks/useAuth';
 import PrivateRoute from '../routes/PrivateRoute';
 import ReversedPrivateRoute from '../routes/ReversedPrivateRoute';
@@ -12,7 +12,7 @@ function App() {
       <Router>
         <Switch>
           <Route exact path='/'>
-            <h1>hello Home page</h1>
+            <h1>hello Home page <Link to='/login'>Go login</Link></h1>
           </Route>
           <Route path='/login' component={SignIn} />
         </Switch>
