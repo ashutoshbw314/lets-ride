a9a1c19d51ca9112304841a205e3f59a6ec305db
Ashutosh Biswas <ashutoshbw314@gmail.com>
2021-03-21 23:02:11 +0600

    fixed some styling issue

--- src/components/RideDetailPage/MyRide.jsx
+++ src/components/RideDetailPage/MyRide.jsx
@@ -51,8 +51,8 @@ function MyRide({vehicle}) {
   }
 
   return (
-    <div className='w-full px-8 py-8 mx-auto lg:max-w-md grid-rows-2 lg:max-w-6xl gap-2 lg:px-12 grid lg:grid-cols-4'>
-      <div className='float-left px-6 py-8 bg-white rounded-lg shadow row-span-1 bg-opacity-90 sm:px-10 lg:col-span-1'>
+    <div className='w-full px-8 py-8 mx-auto lg:max-w-md grid-rows-2 lg:max-w-6xl gap-2 lg:px-12 grid lg:grid-cols-6'>
+      <div className='float-left px-6 py-8 bg-white rounded-lg shadow row-span-1 bg-opacity-90 sm:px-10 lg:col-span-2'>
         {
           result === null ? 
             <form
@@ -92,30 +92,31 @@ function MyRide({vehicle}) {
               />
             </form> :
             <div>
-              <div className='py-5 mb-3 bg-indigo-200 rounded-lg'>
-                <ul className='ml-8 list-disc'>
+              <div style={{backgroundImage: 'linear-gradient(to bottom, #136a8a, #267871)'}} className='py-5 mb-4 text-white rounded-lg'>
+                <ul className='p-0 ml-0 text-center list-none'>
                   <li>{result.from}</li>
+                  <li className='flex justify-center'>
+                    <svg xmlns="http://www.w3.org/2000/svg" width='20' height='20' fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 13l-7 7-7-7m14-8l-7 7-7-7" />
+</svg>
+                  </li>
                   <li>{result.to}</li>
                 </ul>
               </div>
-              <div>
+              <div className='my-6'>
                 {
                   result.details.map((info, i) => {
-                    return <div key={i} className='mb-3 grid grid-cols-3'>
-                      <div className='items-center grid grid-cols-2'>
-                        <img className='rounded-full w-14' src={result.thumbnail}/>
-                        <p className='ml-2'>
+                    return <div key={i} className='px-4 mb-3 grid gap-2 grid-cols-12'>
+                      <img className='rounded-full col-span-2' src={result.thumbnail}/>
+                      <div className='flex items-center col-span-3'>
                           {result.vehicle[0].toUpperCase() + result.vehicle.slice(1)}
-                        </p>
                       </div>
-                      <div className='items-center grid grid-cols-2'>
-                        <img className='w-10' src={userLogo}/>
+                      <div className='flex items-center col-span-2'>
+                        <img className='' src={userLogo}/>
                         {info.persons}
                       </div>
-                      <div className='items-center grid grid-cols-2'>
-                        <p className='text-right'>
+                      <div className='flex items-center justify-end col-span-5'>
                           {'$' + info.price}
-                        </p>
                       </div>
                     </div>
                   })
@@ -128,7 +129,7 @@ function MyRide({vehicle}) {
             </div>
         }
       </div>
-      <div className='lg:col-span-3 row-span-1'>
+      <div className='lg:col-span-4 row-span-1'>
         <MapContainer className='w-full h-full' center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
           <TileLayer
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
